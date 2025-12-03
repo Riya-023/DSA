00c3096e00744bfb1b90a52365402e05c02ddfe0
Riya <168212000+Riya-023@users.noreply.github.com>
2025-12-03 18:06:24 +0530

    Time: 1912 ms (5.08%), Space: 55.5 MB (30.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //brute
-        //tc=O(n^2)
-        // Sc=O(1)
         int n = nums.length;
-        int count =0;
-        for(int i=0; i<n; i++){
+
+        for(int i =0; i<n; i++){
+            int count =0;
             for(int j=0; j<n; j++){
-                if(nums[i]== nums[j]) count++;
+                if(nums[i] == nums[j]) count ++;
             }
-            if(count > n/2) return nums[i];
-            else count =0;
+            if(count > (n/2)) return nums[i];
         }
         return -1;
     }
